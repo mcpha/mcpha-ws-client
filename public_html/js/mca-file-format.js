40aef1de78d2c232bc58692d7e55bfd2aead7899
John Preston <byhisdeeds@gmail.com>
2017-02-25 11:21:03 -0500

    Added download data code to save in plain text, json text and Ortec Maestro chn formats.

--- public_html/js/mca-file-format.js
+++ public_html/js/mca-file-format.js
@@ -14,33 +14,49 @@
  * limitations under the License.
  */
 
-function dump_mca_data(data, format, filename) {
+function dump_mca_data(data, rois, format, filename) {
   if (format === "ortec-chn") {
-    return dump_mca_data_ortec_chn(data, filename);
+    return dump_mca_data_ortec_chn(data, rois, filename);
   } else if (format === "plain-txt") {
-    return dump_mca_data_plain_text(data, filename);
+    return dump_mca_data_plain_text(data, rois, filename);
   } else if (format === "json-txt") {
-    return dump_mca_data_json_text(data, filename);
+    return dump_mca_data_json_text(data, rois, filename);
   }
 }
 
-function dump_mca_data_json_text(data, filename) {
+function dump_mca_data_json_text(data, rois, filename) {
+  var json = {};
+  var d = [];
+  for (var i=0; i<data.length; i++) {
+    d.push(data[i][1]);
+  }
+  json.data = d;
+  json.nchannels = data.length;
+  json.description = "MCA channel data";
   var obj = {
     file:  filename+".json",
-    payload: "data:text/json,{'x':1234}"
+    payload: "data:text/json," + JSON.stringify(json)
   };
   return obj;
 }
 
-function dump_mca_data_plain_text(data, filename) {
+function dump_mca_data_plain_text(data, rois, filename) {
+  var buf = "data:text/plain,# MCA data for " + data.length +
+            " channels%0D# ROI data%0D" +
+            rois[0].count + "%0D" +
+            rois[1].count + "%0D" +
+            rois[2].count + "%0D# Channel data";
+  for (var i=0; i<data.length; i++) {
+    buf += "%0D" + data[i][1];
+  }
   var obj = {
     file:  filename+".txt",
-    payload: "data:text/plain,abcdefg"
+    payload: buf
   };
   return obj;
 }
 
-function dump_mca_data_ortec_chn(data, filename) {
+function dump_mca_data_ortec_chn(data, rois, filename) {
   var obj = {
     file:  filename+".chn",
     payload: "data:application/octet-stream;base64," + uint_8_to_base_64(new Uint8Array([65, 66, 67, 68]))
